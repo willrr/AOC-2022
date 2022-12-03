3d25eb0fd8a395f3bde2bf78af1cd8415865f9af
Will Rose <will@willrr.co.uk>
2022-12-03 16:07:30 +0000

    Day 3 part 2

--- 3/day3.py
+++ 3/day3.py
@@ -7,22 +7,27 @@ def rucksackInput(input):
     for line in input:
         count += 1
         line = line.strip()
-        input1, input2 = splitString(line)
-        for x in input1:
-            for y in input2:
-                if x == y:
-                    commonChar = y
-                    break
-        if ord(commonChar) in range(65, 91):
-            subtract = 64
-            subtract = subtract - 26
-        else:
-            subtract = 96
-        output += ord(commonChar)-subtract
+        if count % 3 == 0:
+            input1 = input[count-3].strip()
+            input2 = input[count-2].strip()
+            input3 = input[count-1].strip()
+            for x in input1:
+                for y in input2:
+                    if x == y:
+                        for z in input3:
+                            if z == y:
+                                commonChar = y
+                                break
+            if ord(commonChar) in range(65, 91):
+                subtract = 64
+                subtract = subtract - 26
+            else:
+                subtract = 96
+            output += ord(commonChar)-subtract
     return output
 
-def splitString(string):
-    input1, input2 = string[:len(string)//2], string[len(string)//2:]
-    return input1, input2
+#def splitString(string):
+#    input1, input2 = string[:len(string)//2], string[len(string)//2:]
+#    return input1, input2
 
 print(rucksackInput(input))
