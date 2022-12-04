8bf3adcec398dbab474d1972c33d16b158feb6c1
Will Rose <will@willrr.co.uk>
2022-12-04 16:17:52 +0000

    Remove debug

--- 4/day4.py
+++ 4/day4.py
@@ -12,10 +12,10 @@ def assignmentInput(input):
         set2value2 = int(set1[1].split("-")[1])
         if set1value1 <= set2value2 and set1value2 >= set2value1:
             count += 1
-            print(str(set1value1) + "-" + str(set1value2) + " is fit in by " + str(set2value1) + "-" + str(set2value2))
+            #print(str(set1value1) + "-" + str(set1value2) + " is fit in by " + str(set2value1) + "-" + str(set2value2))
         elif set1value1 >= set2value2 and set1value2 <= set2value1:
             count += 1
-            print(str(set1value1) + "-" + str(set1value2) + " fits in " + str(set2value1) + "-" + str(set2value2))
+            #print(str(set1value1) + "-" + str(set1value2) + " fits in " + str(set2value1) + "-" + str(set2value2))
     return count
 
 print(assignmentInput(input))
